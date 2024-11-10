eb1f270f23f7cffe514fc55c73eba70ffc7a84be
sayalitarle <162470535+sayalitarle@users.noreply.github.com>
2024-11-10 12:41:29 +0530

    NonRepeatingChar

---
+++ NMonRepeatingChar.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+      
+         unordered_map<char, int> freq;  
+
+        
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        
+        for (char c : s) {
+            if (freq[c] == 1) {
+                return c;  
+            }
+        }
+
+        return '$';  
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
